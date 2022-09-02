35cbc2c4091e22e0f9966ea0148dd3099f429738
joaodiazz <joaogomesdias2001@gmail.com>
2022-09-02 20:19:48 -0300

    arrumei coisas

---
+++ README.md
@@ -0,0 +1,1 @@
+# idk só um teste
--- edit/sketch.js
+++ edit/sketch.js
@@ -1,3 +1,6 @@
+let x = 100;
+let y = 100;
+
 function setup() {
   createCanvas(400, 400);
 }
@@ -5,7 +8,17 @@ function setup() {
 function draw() {
   background(220);
   fill("#333");
-  circle(30, 30, 40);
+  circle(x, y, 40);
   square(200, 200, 100);
   stroke("#f00");
+
+  if (keyIsDown(LEFT_ARROW)) {
+    x = x - 10;
+  } else if (keyIsDown(RIGHT_ARROW)) {
+    x = x + 10;
+  } else if (keyIsDown(UP_ARROW)) {
+    y = y - 10;
+  } else if (keyIsDown(DOWN_ARROW)) {
+    y = y + 10;
+  }
 }
